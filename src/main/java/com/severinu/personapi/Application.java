e1cdde8e1d433c477d4b895f3e736a12166f65f7
seweryn <sewerynmichota@gmail.com>
2025-05-19 21:46:15 +0100

    Inc. app version

--- src/main/java/com/severinu/personapi/Application.java
+++ src/main/java/com/severinu/personapi/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("App v1.0.1");
+        System.out.println("App v1.0.2");
     }
 
 }
